5f6e2521e17ac475f70c00157e8f8237dd0491b3
TedSmi <ted@flibb.net>
2025-02-20 18:59:42 -0800

    Added bounce pads

--- index.js
+++ index.js
@@ -13,12 +13,38 @@ let backgroundColor = new THREE.Color(0x000000);
 scene.background = backgroundColor;
 
 // Player object 
-let height = 0.5;
+let player_height = 0.5;
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const player = new THREE.Mesh(geometry, material);
-player.position.y = height;
+player.position.y = player_height;
 scene.add(player);
+
+// First Bounce Pad object
+let bouncePad_height = 0.2;
+let bouncePad_1_starting_x_pos = 20;
+const bp_geometry = new THREE.BoxGeometry(1, 0.4, 1);
+const bp_material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const bouncePad_1 = new THREE.Mesh(bp_geometry, bp_material);
+bouncePad_1.position.y = bouncePad_height;
+bouncePad_1.position.x = bouncePad_1_starting_x_pos;
+scene.add(bouncePad_1);
+
+// Second Bounce Pad object
+let bouncePad_2_starting_x_pos = 39;
+const bouncePad_2 = new THREE.Mesh(bp_geometry, bp_material);
+bouncePad_2.position.y = bouncePad_height;
+bouncePad_2.position.x = bouncePad_2_starting_x_pos;
+scene.add(bouncePad_2);
+
+
+// Third Bounce Pad object
+let bouncePad_3_starting_x_pos = 49;
+const bouncePad_3 = new THREE.Mesh(bp_geometry, bp_material);
+bouncePad_3.position.y = bouncePad_height;
+bouncePad_3.position.x = bouncePad_3_starting_x_pos;
+scene.add(bouncePad_3);
+
 
 // Floor
 // const floorGeometry = new THREE.PlaneGeometry(200, 1);
@@ -50,6 +76,7 @@ function animate() {
     time += delta_time;
 
     updatePlayer(delta_time);
+    updateBouncePads(delta_time);
 
     // rate of change for hue and lightness
     let hue_roc = 0.05;
@@ -71,6 +98,8 @@ let jump_counter = max_jumps;
 let y_velocity = 0;
 const gravity = -9.8;
 
+let default_obstacle_velocity = 5;
+
 function jump() {
     if (jump_counter > 0) { // if player has 1 or 2 (both) jumps left
         y_velocity += jump_velocity;
@@ -82,9 +111,29 @@ function updatePlayer(delta_time) {
     y_velocity += gravity * delta_time;
     player.position.y += y_velocity * delta_time;
 
-    if ((player.position.y <= height) && (y_velocity <= 0)) {
-        player.position.y = height;
+    if ((player.position.y <= player_height) && (y_velocity <= 0)) {
+        player.position.y = player_height;
         y_velocity = 0;
+        jump_counter = max_jumps;
+    }
+}
+
+
+function updateBouncePads(delta_time) {
+    bouncePad_1.position.x -= default_obstacle_velocity * delta_time;
+    bouncePad_2.position.x -= default_obstacle_velocity * delta_time;
+    bouncePad_3.position.x -= default_obstacle_velocity * delta_time;
+
+    if ((bouncePad_1.position.x <= 1) && (bouncePad_1.position.x >= -1) && (player.position.y <= 0.7)) {
+        y_velocity = 10;
+        jump_counter = max_jumps;
+    }
+    if ((bouncePad_2.position.x <= 1) && (bouncePad_2.position.x >= -1) && (player.position.y <= 0.7)) {
+        y_velocity = 10;
+        jump_counter = max_jumps;
+    }
+    if ((bouncePad_3.position.x <= 1) && (bouncePad_3.position.x >= -1) && (player.position.y <= 0.7)) {
+        y_velocity = 10;
         jump_counter = max_jumps;
     }
 }
